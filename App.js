28f5a6e5b09e58fbab886672c1da9d8fdc6d419a
VKH2801 <20521352@gm.uit.edu.vn>
2022-03-30 09:56:31 +0700

    Fix App.js file

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Platform, SafeAreaView, Easing, StyleSheet, Text, View, Animated } from 'react-native';
+import { StyleSheet, Text, View, Platform, SafeAreaView, Easing, Animated } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
