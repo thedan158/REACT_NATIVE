f35b75705951cfe5ede15e80cb077325ac6958ed
20521289 <20521289@gm.uit.edu.vn>
2022-05-22 09:53:48 +0700

    Update staff information in staff management

--- screens/PermissionManager.js
+++ screens/PermissionManager.js
@@ -1,102 +1,112 @@
-import { StyleSheet, Text, View, TouchableOpacity, Switch, Image } from 'react-native'
-import React from 'react'
-import { ScrollView } from 'react-native-gesture-handler'
-import { CustomCollapsible } from '../custom component/CustomCollapsible'
-import Colors from '../assets/Colors'
-import { useNavigation } from '@react-navigation/core'
-import CustomModal from '../custom component/CustomModal'
-
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Switch,
+  Image,
+} from 'react-native';
+import React from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
+import { CustomCollapsible } from '../custom component/CustomCollapsible';
+import Colors from '../assets/Colors';
+import { useNavigation } from '@react-navigation/core';
+import CustomModal from '../custom component/CustomModal';
 
 const PermissionManager = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   const [visible, setVisible] = React.useState(false);
   return (
-
     <View>
-    {/* Title  */}
+      {/* Title  */}
       <Text style={styles.modalTitle}>Permission Manager</Text>
 
       {/* Permission manager  */}
       <View style={styles.permission}>
         <CustomCollapsible title="Tanhao" subTitle="T" />
-        <CustomCollapsible title="Tanhao" subTitle="This option will allow ... to ..... abcb xnxn x n
+        <CustomCollapsible
+          title="Tanhao"
+          subTitle="This option will allow ... to ..... abcb xnxn x n
 xz nx znx nx nz x bzm xnz bm xnbzm xnzmx nz x
-zxn mbz nxm z nx mz  xz"/>
-        <CustomCollapsible title="Tanhao" subTitle="This option will allow ... to ..... abcb xnxn x n
+zxn mbz nxm z nx mz  xz"
+        />
+        <CustomCollapsible
+          title="Tanhao"
+          subTitle="This option will allow ... to ..... abcb xnxn x n
 xz nx znx nx nz x bzm xnz bm xnbzm xnzmx nz x
-zxn mbz nxm z nx mz  xz"/>
+zxn mbz nxm z nx mz  xz"
+        />
       </View>
 
       {/* Modal  */}
       <CustomModal visible={visible}>
-       
-        <View style={{alignItems: 'center'}}>
+        <View style={{ alignItems: 'center' }}>
           <Image
             source={require('../assets/icons/check.png')}
-            style={{height: 150, width: 150, marginVertical: 30}}
+            style={{ height: 150, width: 150, marginVertical: 30 }}
           />
         </View>
 
-        <Text style={{marginVertical: 30, fontSize: 20, textAlign: 'center'}}>
+        <Text style={{ marginVertical: 30, fontSize: 20, textAlign: 'center' }}>
           Congratulations registration was successful
         </Text>
         <TouchableOpacity
-        onPress={() => {navigation.navigate('RestaurantManagement'); setVisible(false)} }
-        
-        style={styles.button}>
-            <Text style={styles.buttonText}>OK</Text>
+          onPress={() => {
+            navigation.navigate('TabForOwner');
+            setVisible(false);
+          }}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>OK</Text>
         </TouchableOpacity>
       </CustomModal>
 
-
-
       {/* Button  */}
-      <View >
-      <TouchableOpacity
-        onPress={() => setVisible(true)}
-        style={styles.button}>
-            <Text style={styles.buttonText}>Finish</Text>
+      <View>
+        <TouchableOpacity
+          onPress={() => setVisible(true)}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Finish</Text>
         </TouchableOpacity>
-        </View>
       </View>
-  )
-}
+    </View>
+  );
+};
 
-export default PermissionManager
+export default PermissionManager;
 
 const styles = StyleSheet.create({
-  modalTitle:{
-    fontSize:20,
-    fontWeight:'bold',
-    color:Colors.secondary,
-    textAlignVertical:'center',
-    marginTop:80,
-    textAlign:'center'     
-},
-permission:{
-  marginVertical:40
-},
-button:{
-  backgroundColor:Colors.secondary,
-  width:'80%',
-  padding:15,
-  borderRadius:30,
-  justifyContent:'center',
-  alignItems:'center',
-  elevation:1,
-  alignSelf:'center'
-},
-buttonText:{
-  color:'white',
-  fontWeight:'700',
-  fontSize:16,
-
-},
-header: {
-  width: '100%',
-  height: 40,
-  alignItems: 'flex-end',
-  justifyContent: 'center',
-},
-})
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: Colors.secondary,
+    textAlignVertical: 'center',
+    marginTop: 80,
+    textAlign: 'center',
+  },
+  permission: {
+    marginVertical: 40,
+  },
+  button: {
+    backgroundColor: Colors.secondary,
+    width: '80%',
+    padding: 15,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 1,
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  header: {
+    width: '100%',
+    height: 40,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+});
