38dadffd07a0a6f8975e1683aa7e2366ecb6c9ac
VKH2801 <20521352@gm.uit.edu.vn>
2022-06-07 14:58:05 +0700

    Update Button2Screen, Button3Screen, MenuScreen UI

--- screens/Button2Screen.js
+++ screens/Button2Screen.js
@@ -6,48 +6,205 @@ import {
   Image,
   TouchableOpacity,
   TextInput,
-  KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  FlatList,
 } from "react-native";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import Colors from "../assets/Colors";
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
+const maxWidthConst = windowWidth - 10;
+const imgUserSource = require("../assets/icons/user.png");
+const imgGoBackSource = require("../assets/icons/back.png");
+const icStar = require('../assets/icons/Star.png');
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
-const maxWidth40 = windowWidth - 40;
+const maxWidth40 = windowWidth - 30;
 const imgSearchSource = require("../assets/icons/search.png");
+const DataMenu = [
+  {
+    id: 1,
+    imgSource: require('../assets/images/pizza.jpg'),
+    nameDish: 'Pizza',
+    rating: 4.5,
+    votes: 355,
+    price: 200,
+  },
+  {
+    id: 2,
+    imgSource: require('../assets/images/pizza.jpg'),
+    nameDish: 'Pizza with recommendations',
+    rating: 4.8,
+    votes: 422,
+    price: 253,
+  },
+  {
+    id: 3,
+    imgSource: require('../assets/images/pizza.jpg'),
+    nameDish: 'Saro with recommendations',
+    rating: 4.6,
+    votes: 221,
+    price: 131,
+  },
+  {
+    id: 4,
+    imgSource: require('../assets/images/sarawak-laksa.jpg'),
+    nameDish: 'Sarawak laksa',
+    rating: 4.1,
+    votes: 321,
+    price: 203,
+  },
+];
 
-const Button2Screen = () => {
+
+const Button2Screen = ({ navigation }) => {
+  const [search, setSearch] = useState('');
+  const [masterData, setMasterData] = useState([]);
+  const [dataFromState, setNewData] = useState(DataMenu);
+
+  useEffect(() => {
+    setMasterData(DataMenu);
+    console.log('filteredData is all selected');
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    if (text) {
+      const newData = masterData.filter(function (item) {
+        const itemData = item.nameDish
+          ? item.nameDish.toLowerCase()
+          : ''.toUpperCase();
+        const textData = text.toLowerCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setNewData(newData);
+      setSearch(text);
+    } else {
+      setNewData(masterData);
+      setSearch(text);
+    }
+  };
+
+  function HeaderViewTab({ HeaderText }) {
+    return (
+      <View style={styles.containerHeaderViewTab}>
+        <TouchableOpacity
+          style={styles.btnGoBack}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
+          <Image source={imgGoBackSource} style={styles.imgGoBackStyle} />
+        </TouchableOpacity>
+
+        <Text style={styles.txtHeaderViewTab}>{HeaderText}</Text>
+        <TouchableOpacity style={styles.btnUserStyle}>
+          <Image source={imgUserSource} style={styles.imgUserStyle} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function InformationViewTab({ TextInFo1, TextInFo2 }) {
+    return (
+      <View style={styles.containerInfoViewTab}>
+        <Text
+          style={{
+            color: "#fff",
+            fontSize: 18,
+            alignSelf: "center",
+          }}
+        >
+          {TextInFo1}
+        </Text>
+        <Text>{TextInFo2}</Text>
+      </View>
+    );
+  }
+
+  function SearchBarViewComponent() {
+    
+    return (
+      <View style={styles.containerSearchViewComponent}>
+        <View style={{
+          alignSelf: "center",
+          flexDirection: "row",
+          justifyContent: 'flex-end',
+          alignItems: "center",
+          direction: "inherit",
+          flexWrap: "wrap-reverse",
+          flex: 1,
+          alignSelf: 'center',
+          maxWidth: "95%",
+          marginBottom: '2%',
+          paddingHorizontal: '5%',
+        }}>
+          <TouchableOpacity style={styles.btnSearchStyle}>
+            <Image source={imgSearchSource} style={styles.imgSearchStyle} />
+          </TouchableOpacity>
+          <TextInput
+            placeholder="Search Menu..."
+            value={search}
+            onChangeText={(text) => searchFilterFunction(text)}
+            underlineColorAndroid="transparent"
+            style={{
+              flex: 9,
+            }}
+          ></TextInput>
+        </View>
+        
+      </View>
+    );
+  }
+
+  const FlatListItem = ({ item }) => {
+    return (
+      <View style={styles.containerItemFlatList}>
+        <View style={styles.containerImageItem}>
+          <Image source={item.imgSource} style={styles.imgSourceItem} />
+        </View>
+
+        <View style={styles.containerInfoItem}>
+          <Text style={styles.txtNameDishItem}>{item.nameDish}</Text>
+          <View style={styles.containerRatingItem}>
+            <Text style={styles.txtPriceItemInfo2}>${item.price}</Text>
+            <Image source={icStar} style={styles.imgStarItem} />
+
+            <Text style={styles.txtRatingItem}>{item.rating}</Text>
+            <Text>({item.votes})</Text>
+          </View>
+
+          <View style={styles.containerPriceItem}></View>
+        </View>
+      </View>
+    );
+  };
+
   return (
-    <KeyboardAwareScrollView 
-        style={{flex: 1}}
-
-    >
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <LinearGradient
           style={styles.container}
           colors={[Colors.FrenchRaspberry, Colors.ParadisePink]}
           start={{ x: 0, y: 0 }}
           end={{ x: 1, y: 0 }}
         >
-          <View style={styles.containerHeaderSearch}>
-            <View style={styles.containerSearchingSection}>
-              <TouchableOpacity style={styles.btnSearch}>
-                <Image style={styles.imgSearch} source={imgSearchSource} />
-              </TouchableOpacity>
-              <TextInput
-                placeholder="Search..."
-                style={styles.txtInputSearch}
-              />
-            </View>
+          {HeaderViewTab({ HeaderText: "Large Discount" })}
+          {InformationViewTab({ TextInFo1: "60% Off today!!" })}
+          {SearchBarViewComponent()}
+          <View style={styles.containerDevideLine}></View>
+          <View style={styles.containerInfoItem}>
+            <FlatList
+              data={dataFromState}
+              renderItem={({ item, index }) => {
+                return <FlatListItem item={item} index={index} />;
+              }}
+              keyExtractor={(item) => item.id}
+              showsVerticalScrollIndicator={false}
+              showsHorizontalScrollIndicator={false}
+            />
+
           </View>
-          <View style={styles.containerInfoItem}></View>
         </LinearGradient>
-      </TouchableWithoutFeedback>
-    </KeyboardAwareScrollView>
   );
 };
 
@@ -58,44 +215,161 @@ const styles = StyleSheet.create({
     flex: 1,
     width: windowWidth,
     height: windowHeight,
-    paddingTop: "8%",
+    paddingTop: "0%",
   },
-  containerHeaderSearch: {
+  containerHeaderViewTab: {
     flex: 1,
     flexDirection: "row",
-    maxWidth: maxWidth40,
+    height: 60,
+    width: windowWidth,
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexWrap: "wrap-reverse",
+  },
+  containerDevideLine: {
+    height: 1,
+    width: windowWidth - 15,
+    marginTop: '4%',
+    marginBottom: '5%',
+    backgroundColor: '#AFAFAF',
+    alignSelf: 'center',
+  },
+  containerItemFlatList: {
+    width: windowWidth - 40,
+    height: '100%',
+    paddingHorizontal: '5%',
+    backgroundColor: '#FFFFFF',
+    paddingTop: 0,
+    marginVertical: '2%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    paddingBottom: '1.5%',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+
+    elevation: 4,
+    flex: 1,
+  },
+  containerImageItem: {
+    flex: 2,
+    marginBottom: '1%',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  containerSearchViewComponent: {
+    height: 40,
+    width: windowWidth,
+    maxWidth: "85%",
+    alignSelf: "center",
+    borderRadius: 15,
+    backgroundColor: '#fff',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  containerInfoViewTab: {
+    height: 70,
+    width: windowWidth,
+    maxWidth: "110%",
     justifyContent: "center",
+    alignItems: "center",
     alignContent: "center",
+    paddingHorizontal: "5%",
   },
   containerInfoItem: {
-    flex: 9,
+    flex: 1,
   },
-  containerSearchingSection: {
-    flex: 4,
-    flexDirection: "row",
-    maxWidth: "90%",
-    borderRadius: 40,
-    alignSelf: "center",
-    backgroundColor: Colors.lightGray,
-    height: "50%",
+  containerInfoItem:{
+    flex: 7,
+
+  },
+  containerRatingItem: {
+    flexDirection: 'row',
+    flex: 2,
+    marginBottom: '3%',
+  },
+  txtPriceItemInfo2: {
+    color: '#EF5B5B',
+    marginRight: '55%',
+  },
+  txtHeaderViewTab: {
+    color: "#fff",
+    fontSize: 25,
+    fontWeight: "bold",
+    paddingTop: "0%",
+  },
+  txtNameDishItem: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: '2%',
+  },
+  txtRatingItem: {
+    color: '#EF5B5B',
+    marginHorizontal: 5,
+  },
+  containerPriceItem: {
+    flex: 1,
+  },
+  btnSearchStyle: {
+    width: 20,
+    height: 20,
+    flex: 1,
+
+  },
+  imgSearchStyle: {
+    width: 20,
+    height: 20,
+    resizeMode: "cover",
+  },
+  imgStarItem: {
+    height: 15,
+    width: 15,
+  },
+  btnGoBack: {
+    height: 30,
+    width: 30,
     justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
   },
-  imgSearch: {
-    width: "50%",
-    height: "50%",
-    flex: 1,
+  btnUserStyle: {
+    height: 30,
+    width: 30,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  imgGoBackStyle: {
+    height: 25,
+    width: 25,
+    resizeMode: "cover",
     alignSelf: "center",
-    resizeMode: "contain",
   },
-  btnSearch: {
+  imgSourceItem: {
+    resizeMode: 'cover',
+    margin: '2%',
+    borderRadius: 15,
+    height: 150,
+    width: windowWidth - 50,
+    alignSelf: 'center',
     flex: 1,
-    width: "50%",
-    height: "50%",
-    alignSelf: "center",
-    right: "230%",
-    marginRight: "3%",
   },
-  txtInputSearch: {
-    right: "500%",
+  imgUserStyle: {
+    height: 25,
+    width: 25,
+    resizeMode: "cover",
+    alignSelf: "center",
   },
 });
