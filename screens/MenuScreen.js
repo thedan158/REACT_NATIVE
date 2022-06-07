03f27dee219c6df97f9f08559a11cf75b1d636c0
huyy802 <20521401@gm.uit.edu.vn>
2022-06-07 16:07:01 +0700

    feature-getAllFood

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -8,70 +8,101 @@ import {
   TextInput,
   KeyboardAvoidingView,
   FlatList,
-} from 'react-native';
-import React, { useState, useEffect } from 'react';
-import Colors from '../assets/Colors';
-import { LinearGradient } from 'expo-linear-gradient';
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import Colors from "../assets/Colors";
+import { LinearGradient } from "expo-linear-gradient";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-const icBack = require('../assets/icons/back.png');
-const icUser = require('../assets/icons/user.png');
-const icSearch = require('../assets/icons/search.png');
-const icStar = require('../assets/icons/Star.png');
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+const icBack = require("../assets/icons/back.png");
+const icUser = require("../assets/icons/user.png");
+const icSearch = require("../assets/icons/search.png");
+const icStar = require("../assets/icons/Star.png");
 const maxWidthConst = windowWidth - 10;
 
 const DataMenu = [
   {
     id: 1,
-    imgSource: require('../assets/images/pizza.jpg'),
-    nameDish: 'Pizza',
+    // imgSource: require("../assets/images/pizza.jpg"),
+    nameDish: "Pizza",
     rating: 4.5,
     votes: 355,
     price: 200,
   },
   {
     id: 2,
-    imgSource: require('../assets/images/pizza.jpg'),
-    nameDish: 'Pizza with recommendations',
+    // imgSource: require("../assets/images/pizza.jpg"),
+    nameDish: "Pizza with recommendations",
     rating: 4.8,
     votes: 422,
     price: 253,
   },
   {
     id: 3,
-    imgSource: require('../assets/images/pizza.jpg'),
-    nameDish: 'Saro with recommendations',
+    // imgSource: require("../assets/images/pizza.jpg"),
+    nameDish: "Saro with recommendations",
     rating: 4.6,
     votes: 221,
     price: 131,
   },
   {
     id: 4,
-    imgSource: require('../assets/images/sarawak-laksa.jpg'),
-    nameDish: 'Sarawak laksa',
+    // imgSource: require("../assets/images/sarawak-laksa.jpg"),
+    nameDish: "Sarawak laksa",
     rating: 4.1,
     votes: 321,
     price: 203,
   },
 ];
+const DataMenu1 = [
+  {
+    id: 1,
+    // imgSource: require("../assets/images/pizza.jpg"),
+    nameDish: "Pizza",
+    rating: 4.5,
+    votes: 355,
+    price: 200,
+  },
+];
 
 const MenuScreen = ({ navigation }) => {
-  const [search, setSearch] = useState('');
-  const [masterData, setMasterData] = useState([]);
-  const [dataFromState, setNewData] = useState(DataMenu);
-
+  const [dataFromState, setNewData] = useState([]);
   useEffect(() => {
-    setMasterData(DataMenu);
-    console.log('filteredData is all selected');
-  }, []);
+    const getData = async () => {
+      const userLoginData = await AsyncStorage.getItem("userLoginData");
+      const user = JSON.parse(userLoginData);
+      console.log("username: " + user.username);
+      const res = await axios.post(
+        `https://foody-uit.herokuapp.com/food/getAllFoodOfRestaurant`,
+        {
+          username: user.username,
+        }
+      );
 
+      const { success, message } = res.data;
+      console.log(message);
+      console.log(success);
+
+      console.log("filteredData is all selected");
+      setNewData(message);
+    };
+
+    getData().catch((err) => console.log(err));
+  }, []);
+  const [search, setSearch] = useState("");
+  const [masterData, setMasterData] = useState([]);
+  useEffect(() => {
+    setMasterData(dataFromState);
+  }, []);
   const searchFilterFunction = (text) => {
     if (text) {
       const newData = masterData.filter(function (item) {
         const itemData = item.nameDish
           ? item.nameDish.toLowerCase()
-          : ''.toUpperCase();
+          : "".toUpperCase();
         const textData = text.toLowerCase();
         return itemData.indexOf(textData) > -1;
       });
@@ -87,17 +118,17 @@ const MenuScreen = ({ navigation }) => {
     return (
       <View style={styles.containerItemFlatList}>
         <View style={styles.containerImageItem}>
-          <Image source={item.imgSource} style={styles.imgSourceItem} />
+          {/* <Image source={item.imgSource} style={styles.imgSourceItem} /> */}
+          <Image
+            source={require("../assets/images/sarawak-laksa.jpg")}
+            style={styles.imgSourceItem}
+          />
         </View>
 
         <View style={styles.containerInfoItem}>
-          <Text style={styles.txtNameDishItem}>{item.nameDish}</Text>
+          <Text style={styles.txtNameDishItem}>{item.name}</Text>
           <View style={styles.containerRatingItem}>
             <Text style={styles.txtPriceItemInfo2}>${item.price}</Text>
-            <Image source={icStar} style={styles.imgStarItem} />
-
-            <Text style={styles.txtRatingItem}>{item.rating}</Text>
-            <Text>({item.votes})</Text>
           </View>
 
           <View style={styles.containerPriceItem}></View>
@@ -108,7 +139,7 @@ const MenuScreen = ({ navigation }) => {
 
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.containerKeyboardAvoid}
     >
       <LinearGradient
@@ -174,7 +205,7 @@ const styles = StyleSheet.create({
     height: windowHeight,
     width: windowWidth,
     paddingTop: 20,
-    paddingHorizontal: '3%',
+    paddingHorizontal: "3%",
     flex: 1,
   },
   containerKeyboardAvoid: {
@@ -188,31 +219,31 @@ const styles = StyleSheet.create({
   containerDevideLine: {
     flex: 0.01,
     width: maxWidthConst,
-    height: '0.1%',
-    marginTop: '0%',
-    marginBottom: '0%',
-    backgroundColor: '#AFAFAF',
-    alignSelf: 'center',
+    height: "0.1%",
+    marginTop: "0%",
+    marginBottom: "0%",
+    backgroundColor: "#AFAFAF",
+    alignSelf: "center",
   },
   containerMenuInfo: {
     flex: 3,
-    justifyContent: 'center',
-    marginTop: '1.5%',
-    backgroundColor: 'transparent',
-    marginBottom: '0%',
+    justifyContent: "center",
+    marginTop: "1.5%",
+    backgroundColor: "transparent",
+    marginBottom: "0%",
   },
   containerItemFlatList: {
     width: windowWidth - 40,
-    height: '100%',
-    paddingHorizontal: '5%',
-    backgroundColor: '#FFFFFF',
+    height: "100%",
+    paddingHorizontal: "5%",
+    backgroundColor: "#FFFFFF",
     paddingTop: 0,
-    marginVertical: '2%',
-    alignSelf: 'center',
-    justifyContent: 'center',
+    marginVertical: "2%",
+    alignSelf: "center",
+    justifyContent: "center",
     borderRadius: 15,
-    paddingBottom: '1.5%',
-    shadowColor: '#000',
+    paddingBottom: "1.5%",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -225,30 +256,30 @@ const styles = StyleSheet.create({
   },
   containerImageItem: {
     flex: 2,
-    marginBottom: '1%',
-    justifyContent: 'center',
-    alignContent: 'center',
+    marginBottom: "1%",
+    justifyContent: "center",
+    alignContent: "center",
   },
   containerInfoItem: {
     flex: 1,
   },
   containerRatingItem: {
-    flexDirection: 'row',
+    flexDirection: "row",
     flex: 2,
-    marginBottom: '3%',
+    marginBottom: "3%",
   },
   containerSearchView: {
-    flexDirection: 'row',
-    height: '40%',
+    flexDirection: "row",
+    height: "40%",
     width: windowWidth - 80,
-    justifyContent: 'center',
-    alignSelf: 'center',
-    alignItems: 'center',
-    marginTop: '3%',
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+    marginTop: "3%",
     maxWidth: windowWidth - 80,
     borderRadius: 45,
-    backgroundColor: '#fff',
-    shadowColor: '#000',
+    backgroundColor: "#fff",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 3,
@@ -260,76 +291,76 @@ const styles = StyleSheet.create({
   },
   containerHeaderTab: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'center',
-    paddingTop: '0%',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    paddingTop: "0%",
   },
   containerPriceItem: {
     flex: 1,
   },
   containerHeaderInfoSections: {
     flex: 1.5,
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
     maxWidth: windowWidth - 10,
     width: windowWidth - 0,
-    paddingHorizontal: '0%',
+    paddingHorizontal: "0%",
   },
   imgBack: {
     height: 30,
     width: 30,
-    marginRight: '0%',
-    resizeMode: 'contain',
+    marginRight: "0%",
+    resizeMode: "contain",
     flex: 1,
   },
   imgUser: {
     height: 30,
     width: 30,
-    marginLeft: '0%',
-    resizeMode: 'contain',
+    marginLeft: "0%",
+    resizeMode: "contain",
     flex: 1,
   },
   txtMenuHeader: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 30,
-    paddingTop: '0%',
+    paddingTop: "0%",
     flex: 4,
-    alignSelf: 'center',
-    alignItems: 'center',
-    fontWeight: 'bold',
-    paddingHorizontal: '6%',
+    alignSelf: "center",
+    alignItems: "center",
+    fontWeight: "bold",
+    paddingHorizontal: "6%",
   },
   txtInfo1: {
-    color: '#fff',
-    alignContent: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignSelf: 'center',
+    color: "#fff",
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
   },
   txtRatingItem: {
-    color: '#EF5B5B',
+    color: "#EF5B5B",
     marginHorizontal: 5,
   },
   txtPriceItemInfo: {
-    color: '#EF5B5B',
+    color: "#EF5B5B",
   },
   txtPriceItemInfo2: {
-    color: '#EF5B5B',
-    marginRight: '55%',
+    color: "#EF5B5B",
+    marginRight: "55%",
   },
   txtInfo2: {
-    color: '#fff',
-    alignContent: 'center',
-    alignSelf: 'center',
+    color: "#fff",
+    alignContent: "center",
+    alignSelf: "center",
   },
   txtNameDishItem: {
     fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: '2%',
+    fontWeight: "bold",
+    marginBottom: "2%",
   },
   icSearch: {
     height: 20,
@@ -337,12 +368,12 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   imgSourceItem: {
-    resizeMode: 'cover',
-    margin: '2%',
+    resizeMode: "cover",
+    margin: "2%",
     borderRadius: 15,
     height: 150,
     width: windowWidth - 50,
-    alignSelf: 'center',
+    alignSelf: "center",
     flex: 1,
   },
   imgStarItem: {
